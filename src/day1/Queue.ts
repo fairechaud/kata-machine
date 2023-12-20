a872b093fe58ba9fc1159ce83d2d1e82253b825d
fer <rodriguezcortes.fer@gmail.com>
2023-12-20 15:16:32 +1300

    Completed Queue script and passed tests

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,51 @@
+type Node<T> = {
+    value: T,
+    next?: Node<T>,
+}
+
 export default class Queue<T> {
     public length: number; 
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
     
 
     
     enqueue(item: T): void {
-
-}
+    // add it to the queue (insert all the way at the back)
+        const node = {value: item} as Node<T>;
+        this.length++;
+        if (!this.tail){
+            this.tail = this.head = node; 
+            return;
+        }
+        this.tail.next = node;
+        this.tail = node;
+    }
     deque(): T | undefined {
+    // remove something from the queue (pop from the front)
+        if (!this.head){
+            return undefined;
+        }
+        this.length--;
 
-}
+        //step 1: save and update head
+        const head = this.head;
+        this.head = this.head.next;
+        //step2: free up memory here/return value
+        
+        head.next = undefined;
+
+        if (this.length === 0){
+            this.head = this.tail = undefined;
+        }
+        return head.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
