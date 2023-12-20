4513987d4203c70d81f2cdbfbc1793d30d357c8a
fer <rodriguezcortes.fer@gmail.com>
2023-12-21 12:13:17 +1300

    Completed and tested the first 3 algorithms

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,16 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let hi = haystack.length - 1;
+    let lo = 0;
 
+    do{
+        let mid = Math.floor((lo + hi)/2);
+        if(haystack[mid] === needle){
+            return true;
+        }else if(haystack[mid] < needle){
+            lo = mid + 1;
+        }else{
+            hi = mid - 1;
+        }
+    }while(lo <= hi)
+    return false;
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,18 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-
+    // step 1, find a '1' 
+    const jump = Math.floor(Math.sqrt(breaks.length));
+    let i = jump;
+    for (; i < breaks.length; i+=jump){
+        if(breaks[i]){
+            break;
+        }
+    } 
+    // step 2, walk backwards until '0' is found
+    i -= jump;
+    for (let j = 0; j < jump && i < breaks.length; ++j, ++i){
+        if(breaks[i]){
+            return i;
+        }
+    }
+    return -1;
 }
